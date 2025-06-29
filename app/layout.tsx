da94372423f5acbef5da354b6e13e8e92a0e5871
Viraj <virajmane5439@gmail.com>
2025-06-29 16:27:45 +0530

    dark mode default

--- app/layout.tsx
+++ app/layout.tsx
@@ -89,7 +89,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="light"
+          defaultTheme="dark"
           enableSystem={false}
           disableTransitionOnChange={false}
         >
